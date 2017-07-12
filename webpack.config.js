a85737815218386bd8269e8139cbe5fe682f946d
Amaar Esufally <amaar.esufally@gmail.com>
2017-07-11 23:04:31 -0400

    Fixed jumping animation, issue with platform switching

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,7 @@ module.exports = {
     filename: "./lib/bundle.js"
   },
   module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
+
   },
   devtool: 'source-maps',
   resolve: {
